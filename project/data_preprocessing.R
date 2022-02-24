3f479b19018705898a93f5a2f5acebe9be9b9a7a
Jongwon <45282870+jwpark0625@users.noreply.github.com>
2022-02-24 14:04:41 +0900

    Delete project directory

--- project/data_preprocessing.R
+++
@@ -1,8 +0,0 @@
-data1<-read.csv(file="korea_2020.csv")
-data2<-read.csv(file="korea_2021.csv")
-
-# 일시를 기준으로 dat1, dat2 데이터 합치기
-annual_data<- rbind(data1, data2)
-
-#생성 이후 맨 앞의 컬럼 제거 필요
-write.csv(annual_data,file="Korea_weather.csv")
